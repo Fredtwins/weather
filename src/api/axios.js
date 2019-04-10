6b42eb3a819bb98ae35de27a761ebbdcdd7b839e
liangfapeng <Fredtwins@163.com>
2019-04-10 16:52:56 +0800

    修改

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,17 +1,27 @@
 import axios from 'axios'
 
 // let httpUrl = 'http://192.168.1.174:8080/'
-let httpUrl = 'http://10.0.1.184:8080/'
+// let httpUrl = 'http://10.0.1.184:8080/'
 // let httpUrl = 'http://219.132.198.90:8080/'
 // let httpUrl = 'http://192.168.121.175:8080/'
 
 // let httpUrl = 'http://192.168.43.128:8080/'
 
 // 页面接口通用(get)
-export function ajaxGet (url = '', data = {}) {
-  url = `${httpUrl}${url}`
+// export function ajaxGet (url = '', data = {}) {
+//   url = `${httpUrl}${url}`
+//   return axios.get(url, {
+//     params: data,
+//   }).then(res => {
+//     return Promise.resolve(res.data)
+//   }).catch(res => {
+//     return Promise.resolve(res.data)
+//   })
+// }
+export function ajaxGet (URL = '', data = {}) {
+  let url = URL
   return axios.get(url, {
-    params: data,
+    params: data
   }).then(res => {
     return Promise.resolve(res.data)
   }).catch(res => {
--- src/components/article/tornadoResearch/organizationSynopsis.vue
+++ src/components/article/tornadoResearch/organizationSynopsis.vue
@@ -1,21 +1,11 @@
 <template>
   <div class="tornado-introduce">
-<<<<<<< HEAD
     <span class="content" v-html="content"></span>
-=======
-    <!--<span class="title">{{title}}</span>-->
-    <span class="content" v-html="content"></span>
-    <img src="../img/tornado.jpg"/>
->>>>>>> b9eea966b6c640adea089e0f12d3a4e841b641c5
   </div>
 </template>
 <script>
 
-<<<<<<< HEAD
 import { GetTornado } from 'api/article'
-=======
-import { GetTornado } from 'api/tornado'
->>>>>>> b9eea966b6c640adea089e0f12d3a4e841b641c5
 import { ERR_OK } from 'api/config'
 import { errorNotice } from 'common/js/dom'
 
@@ -34,12 +24,7 @@ export default {
       GetTornado()
         .then(res => {
           if (res.code === ERR_OK) {
-<<<<<<< HEAD
             this.content = res.data[0].content
-=======
-            let data = res.articleList[0].content
-            this.content = data
->>>>>>> b9eea966b6c640adea089e0f12d3a4e841b641c5
           }
         })
         .catch(res => {
@@ -54,29 +39,9 @@ export default {
   width: 100%;
   padding: 20px 30px;
   box-sizing: border-box;
-<<<<<<< HEAD
-=======
-  .title {
-    width: 100%;
-    display: block;
-    height: 24px;
-    line-height: 24px;
-    font-size: 22px;
-    text-align: center;
-  }
->>>>>>> b9eea966b6c640adea089e0f12d3a4e841b641c5
   .content{
     width: 100%;
     display: block;
   }
-<<<<<<< HEAD
-=======
-  img {
-    width: 800px;
-    height: 380px;
-    display: block;
-    margin: 0 auto;
-  }
->>>>>>> b9eea966b6c640adea089e0f12d3a4e841b641c5
 }
 </style>
